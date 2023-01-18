062456cceb7eafad8163c65eb6d9bce10f02f7a3
Matt <matt@xx.network>
2023-01-19 01:54:18 +0700

    Registration fixed

--- src/components/views/CodeNameRegistration/index.tsx
+++ src/components/views/CodeNameRegistration/index.tsx
@@ -1,4 +1,4 @@
-import { FC, useState, useEffect } from 'react';
+import { FC, useState, useEffect, useCallback } from 'react';
 import cn from 'classnames';
 
 import { ImportCodeNameLoading, ModalCtaButton } from 'src/components/common';
@@ -11,11 +11,9 @@ const CodenameRegistration: FC = () => {
   const {
     checkRegistrationReadiness,
     cmix,
-    generateIdentities,
-    isReadyToRegister,
-    setIsReadyToRegister
+    generateIdentities
   } = useNetworkClient();
-
+  const [loading, setLoading] = useState(false);
   const [identities, setIdentites] = useState<ReturnType<typeof generateIdentities>>([]);
   const [selectedCodeName, setSelectedCodeName] = useState('');
   const [selectedPrivateIdentity, setSelectedPrivateIdentity] = useState<Uint8Array>();
@@ -30,116 +28,115 @@ const CodenameRegistration: FC = () => {
     }
   }, [firstTimeGenerated, generateIdentities, cmix]);
 
-  if (typeof isReadyToRegister === 'undefined') {
-    return (
-      <div
-        className={cn(
-          'w-full flex flex-col justify-center items-center px-6',
-          s.root
-        )}
+  const register = useCallback(async () => {
+    setLoading(true);
+    setTimeout(async () => {
+      if (selectedPrivateIdentity) {
+        checkRegistrationReadiness(
+          selectedPrivateIdentity,
+          (isReadyInfo) => {
+            if (isReadyInfo.IsReady) {
+              setTimeout(() => {
+                setLoading(false);
+              }, 2500)
+            }
+            setReadyProgress(
+              Math.ceil((isReadyInfo?.HowClose || 0) * 100)
+            );
+          }
+        );
+      }
+      
+    }, 500);
+  }, [checkRegistrationReadiness, selectedPrivateIdentity]);
+
+  return loading ? <ImportCodeNameLoading fullscreen readyProgress={readyProgress} /> : (
+    <div
+      className={cn(
+        'w-full flex flex-col justify-center items-center px-6',
+        s.root
+      )}
+    >
+      <h2 className='mt-9 mb-4'>Find your Codename</h2>
+      <p
+        className='mb-8 text text--sm'
+        style={{
+          color: 'var(--cyan)',
+          lineHeight: '18px',
+          fontSize: '14px',
+          fontWeight: '500',
+          maxWidth: '800px',
+          textAlign: 'center'
+        }}
       >
-        <h2 className='mt-9 mb-4'>Find your Codename</h2>
-        <p
-          className='mb-8 text text--sm'
-          style={{
-            color: 'var(--cyan)',
-            lineHeight: '18px',
-            fontSize: '14px',
-            fontWeight: '500',
-            maxWidth: '800px',
-            textAlign: 'center'
-          }}
+        <span>
+          Codenames are generated on your computer by you. No servers or
+          databases are involved at all.
+        </span>
+        <br />
+        <span>
+          Your Codename is your personally owned anonymous identity shared
+          across every Speakeasy you join. It is private and it can never be
+          traced back to you.
+        </span>
+      </p>
+
+      {identities.length ? (
+        <div
+          className={cn(
+            'grid grid-cols-4 gap-x-4 gap-y-6 overflow-auto',
+            s.codeContainers
+          )}
         >
-          <span>
-            Codenames are generated on your computer by you. No servers or
-            databases are involved at all.
-          </span>
-          <br />
-          <span>
-            Your Codename is your personally owned anonymous identity shared
-            across every Speakeasy you join. It is private and it can never be
-            traced back to you.
-          </span>
-        </p>
-
-        {identities.length ? (
-          <div
-            className={cn(
-              'grid grid-cols-4 gap-x-4 gap-y-6 overflow-auto',
-              s.codeContainers
-            )}
-          >
-            {identities.map((i) => {
-              return (
-                <div
-                  key={i.codename}
-                  className={cn(s.codename, {
-                    [s.selected]: i.codename === selectedCodeName
-                  })}
-                  onClick={() => {
-                    setSelectedCodeName(i.codename);
-                    setSelectedPrivateIdentity(i.privateIdentity);
-                  }}
-                >
-                  <span>{i.codename}</span>
-                </div>
-              );
-            })}
-          </div>
-        ) : (
-          <div className={s.loading}>
-            <div className='w-full h-full flex justify-center items-center'>
-              <Spinner size='lg' />
-            </div>
-          </div>
-        )}
-
-        <div className='flex mb-5 mt-12'>
-          <ModalCtaButton
-            buttonCopy='Discover More'
-            cssClass={s.generateButton}
-            style={{
-              backgroundColor: 'var(--black-1)',
-              color: 'var(--orange)',
-              borderColor: 'var(--orange)'
-            }}
-            onClick={() => {
-              setSelectedCodeName('');
-              setIdentites(generateIdentities(20));
-            }}
-            disabled={!cmix}
-          />
-          <ModalCtaButton
-            buttonCopy='Claim'
-            cssClass={s.registerButton}
-            onClick={async () => {
-              setIsReadyToRegister(false);
-              setTimeout(async () => {
-                if (selectedPrivateIdentity) {
-                  checkRegistrationReadiness(
-                    selectedPrivateIdentity,
-                    (isReadyInfo) => {
-                      setReadyProgress(
-                        Math.ceil((isReadyInfo?.HowClose || 0) * 100)
-                      );
-                    }
-                  );
-                }
-                
-              }, 500);
-            }}
-            disabled={!cmix || selectedCodeName.length === 0}
-          />
+          {identities.map((i) => {
+            return (
+              <div
+                key={i.codename}
+                className={cn(s.codename, {
+                  [s.selected]: i.codename === selectedCodeName
+                })}
+                onClick={() => {
+                  setSelectedCodeName(i.codename);
+                  setSelectedPrivateIdentity(i.privateIdentity);
+                }}
+              >
+                <span>{i.codename}</span>
+              </div>
+            );
+          })}
         </div>
+      ) : (
+        <div className={s.loading}>
+          <div className='w-full h-full flex justify-center items-center'>
+            <Spinner size='lg' />
+          </div>
+        </div>
+      )}
+
+      <div className='flex mb-5 mt-12'>
+        <ModalCtaButton
+          buttonCopy='Discover More'
+          cssClass={s.generateButton}
+          style={{
+            backgroundColor: 'var(--black-1)',
+            color: 'var(--orange)',
+            borderColor: 'var(--orange)'
+          }}
+          onClick={() => {
+            setSelectedCodeName('');
+            setIdentites(generateIdentities(20));
+          }}
+          disabled={!cmix}
+        />
+        <ModalCtaButton
+          buttonCopy='Claim'
+          cssClass={s.registerButton}
+          onClick={register}
+          disabled={!cmix || selectedCodeName.length === 0}
+        />
       </div>
-    );
-  } else if (isReadyToRegister === false) {
-    return (
-      <ImportCodeNameLoading fullscreen readyProgress={readyProgress} />
-    );
-  } else {
-    return null;
-  }
+    </div>
+  );
 };
 
 export default CodenameRegistration;
