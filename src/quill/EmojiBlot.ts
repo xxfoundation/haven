15ef6b2c69e8b5027cf1c787572285aba0dce490
Matt <matt@xx.network>
2023-03-24 01:22:02 +0700

    Added emoji shorthands

---
+++ src/quill/EmojiBlot.ts
@@ -0,0 +1,17 @@
+import Quill from 'quill';
+const Embed = Quill.import('blots/embed');
+
+class EmojiBlot extends Embed {
+  static create(value: string) {
+    const node = super.create();
+    node.classList.add('emoji');
+    node.innerText = value;
+    return node;
+  }
+}
+
+EmojiBlot.blotName = 'emoji';
+EmojiBlot.tagName = 'span';
+EmojiBlot.className = 'myclass';
+
+export default EmojiBlot;
