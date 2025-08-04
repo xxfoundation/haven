8b576e392e7a0fdf1fd008377b4b7681cfd39fae
thisisommore <ommore501@gmail.com>
2025-08-04 17:23:18 +0000

    revert vite.config.ts to main

--- vite.config.ts
+++ vite.config.ts
@@ -13,6 +13,8 @@ export default defineConfig({
       exclude: ['node_modules/**', 'dist/**'],
       failOnError: false,
       failOnWarning: false,
+      cache: true,
+      lintOnStart: true,
       overrideConfigFile: 'eslint.config.js'
     })
   ],
@@ -80,22 +82,7 @@ export default defineConfig({
   },
   server: {
     port: 3000,
-    fs: {
-      // Allow serving files from one level up from the package root
-      allow: ['..'],
-      strict: false
-    },
-    headers: {
-      'Cross-Origin-Opener-Policy': 'same-origin',
-      'Cross-Origin-Embedder-Policy': 'require-corp'
-    },
-    allowedHosts: [
-      '3000-thisisommore-haven-teqe8xrq0zp.ws-eu118.gitpod.io',
-      '3001-debug-thisisommore-haven-teqe8xrq0zp.ws-us118.gitpod.io',
-      '3000-thisisommore-haven-teqe8xrq0zp.ws-us118.gitpod.io',
-      '3000-thisisommore-haven-lgb7hujlja6.ws-us120.gitpod.io',
-      '3000-thisisommore-haven-lgb7hujlja6.ws-eu120.gitpod.io'
-    ]
+    open: true
   },
   resolve: {
     alias: {
